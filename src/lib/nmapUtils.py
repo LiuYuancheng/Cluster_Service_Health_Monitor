546b134e9812a918b94da68b49f704f201aa9acf
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-03-11 23:47:40 +0800

    Update the code document.

--- src/lib/nmapUtils.py
+++ src/lib/nmapUtils.py
@@ -1,43 +1,57 @@
 #-----------------------------------------------------------------------------
 # Name:        nmapUtils.py
 #
-# Purpose:     This module is a extend module of the lib <python-nmap> and the 
-#              nmap software. The module need Nmap to be installed:
-#              https://nmap.org/download
+# Purpose:     This module is a untility module of the lib <python-nmap> to provide
+#              some extend function. The module need netowork scan software Nmap to 
+#              be installed: https://nmap.org/download
 #
 # Author:      Yuancheng Liu
 #
-# Version:     v_0.1
+# Version:     v_0.1.1
 # Created:     2023/03/10
 # Copyright:   n.a
 # License:     n.a
 #-----------------------------------------------------------------------------
+""" Program Design:
+    python-nmap is a python library which helps in using nmap port scanner. It allows 
+    to easilly manipulate nmap scan results and will be a perfect tool for systems 
+    administrators who want to automatize scanning task and reports. The nmapUtils module
+    is a package moudle of python-nmap, it will parse the result of python-nmap and 
+    only provide the result user need.
 
-import nmap
+    <python-nmap> link: https://pypi.org/project/python-nmap/
 
-OPEN_TAG = 'open'
-CLOSE_TAG = 'closed'
-FILTER_TAG = 'filtered'
+"""
+import ipaddress
+import nmap # pip install python-nmap, 
+
+OPEN_TAG = 'open'       # Port opened 
+CLOSE_TAG = 'closed'    # Port closed
+FILTER_TAG = 'filtered' # Port touchable but no reponse or the response can not be recognised by nmap
 UNKNOWN_TAG = 'unknown'
+STATE_UP = 'up'
+STATE_DOWN = 'down'
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class nmapScanner(object):
+    """ A port scanner used to check the port, service state of a target IP. """
 
     def __init__(self) -> None:
+        """ Init the scanner. Example: scanner = nmapScanner()"""
         self.scanner = nmap.PortScanner()
         self.resultDict = None
 
 #-----------------------------------------------------------------------------
     def _parseNmapDict(self, nmapDict, protocalType='tcp', showFiltered=False):
-        """ Convert the Nmap scan result dict to {'<portnum>':(<state>, serverType), ...}
+        """ Convert the Nmap scan result dict to {'<portnum>':(<state>, <serviceType>), ...}
             format.
             Args:
                 nmapDict (dict): nampScanDict[<ip>]
                 protocalType (str, optional): protocalType. Defaults to 'tcp'.
                 showFiltered (bool, optional): whether show 'filtered' port. Defaults to False.
             Returns:
-                _type_: _description_
+                dict: refer to function introduction.
         """
         resultDict = {}
         if protocalType in nmapDict.keys():
@@ -48,13 +62,23 @@ class nmapScanner(object):
                 isopen = state['state'] if 'state' in state.keys() else CLOSE_TAG
                 resultDict[str(port)] = (isopen, serviceName)
         return resultDict
+#-----------------------------------------------------------------------------
+    def getLastScanRawResult(self):
+        return self.resultDict
 
 #-----------------------------------------------------------------------------
     def scanPortDecorator(scanFunction):
+        """ A decorator class to pre-check the target ip address, then call the detailed scan function 
+            to do the port/service scan to update the reuslt.
+            Args:
+                scanFunction (_type_): a scan function same as below parameters config: 
+                    scanFunction(self, target, portInfo, showFiltered=showFiltered)
+        """
         def innerFunc(self, target, portInfo, showFiltered=False):
             self.resultDict = {}
-            if str(target).lower() == 'localhost': target = '127.0.0.1' 
-            self.resultDict = {'target': target, 'state': 'down'}
+            target = '127.0.0.1' if str(target).strip().lower() == 'localhost' else str(target).strip()
+            self.resultDict = {'target': target, 'state': STATE_DOWN}
+            # Call the detail scan function to update the <self.resultDict>
             scanFunction(self, target, portInfo, showFiltered=showFiltered)
             if target in self.scanner.all_hosts():
                 nmapInfo = self.scanner[str(target)]
@@ -67,13 +91,18 @@ class nmapScanner(object):
 #-----------------------------------------------------------------------------
     @scanPortDecorator
     def scanTcpPorts(self, target, portList, showFiltered=False):
-        """ Check a list TCP port state and service 
+        """ Check a list TCP ports' state and service type.
             Args:
-                target (_type_): target IP address
-                portList (_type_): list of int port.
-                showFiltered (bool, optional): whether show 'filtered' port. Defaults to False.
+                target (_type_): target IP address/Url.
+                portList (_type_): list of int ports.
+                showFiltered (bool, optional): whether show the 'filtered' state port. 
+                    Defaults to False.
             Returns:
-                _type_: _description_
+                dict: example:  {   'target': '127.0.0.1', 
+                                    'state': 'up', 
+                                    '134': ('closed', 'ingres-net'), 
+                                    '443': ('open', 'https'), 
+                                    '3000': ('open', 'ppp')}
         """
         for i in portList:
             self.resultDict[str(i)] = (CLOSE_TAG, UNKNOWN_TAG) 
@@ -82,7 +111,9 @@ class nmapScanner(object):
 
     #-----------------------------------------------------------------------------
     def scanTcpPortsOld(self, target, portList, showFiltered=False):
-        """ Same as function function scanTcpPorts(), current not used."""
+        """ Same as function function scanTcpPorts() without decorated, current this 
+            function is not used.
+        """
         if str(target).lower() == 'localhost': target = '127.0.0.1' 
         resultDict = {'target': target, 'state': 'down'}
         for i in portList:
@@ -99,23 +130,23 @@ class nmapScanner(object):
 #-----------------------------------------------------------------------------
     @scanPortDecorator
     def scanPortRange(self, target, portRange, showFiltered=False):
-        """_summary_
+        """ Scan a port range 
 
-        Args:
-            target (_type_): _description_
-            portList (_type_): _description_
-            showFiltered (bool, optional): _description_. Defaults to False.
-
-        Returns:
-            _type_: _description_
+            Args:
+                target (str): target IP address/Url.
+                portRange (tupple): (start port, end port)
+                showFiltered (bool, optional): whether show the 'filtered' state port. 
+                    Defaults to False.
+            Returns:
+                _type_: _description_
         """
         argStr = str(portRange[0])+'-'+str(portRange[1])
         self.scanner.scan(target, argStr)
 
-
     #-----------------------------------------------------------------------------
-    def scanPortRange_old(self, target, portRange, showFiltered=False):
-        """ Same as function function scanTcpPorts(), current not used."""
+    def scanPortRangeOld(self, target, portRange, showFiltered=False):
+        """ Same as function function scanPortRange()without decorated, current this 
+            function is not used."""
         if str(target).lower() == 'localhost': target = '127.0.0.1' 
         resultDict = {'target': target, 'state': 'down'}
         argStr = str(portRange[0])+'-'+str(portRange[1])
@@ -127,6 +158,38 @@ class nmapScanner(object):
                 resultDict.update(self._parseNmapDict(nmapInfo, protocalType='tcp',showFiltered=showFiltered))
         return resultDict
     
+#-----------------------------------------------------------------------------
+    def fastScan(self, target):
+        """ fast Scan a target, same as the cmd: nmap -F <ip>
+        """
+        return self._fastScanTarget(target, None)
+    
+    @scanPortDecorator
+    def _fastScanTarget(self, target, portInfo, showFiltered=False):
+        self.scanner.scan(hosts=target, arguments='-F')
+
+#-----------------------------------------------------------------------------
+    def scanServices(self, target, serviceList):
+        #resultDict = {'target': None, state}
+        
+        
+        self._scanServices(target, serviceList)
+        #for item in resultDict.items():
+        #    key, val = item
+        #    serviceType = val[-1]
+        #    if serviceType in resultDict.keys():
+        #        if not isinstance(resultDict[serviceType], dict):     
+        #            resultDict[serviceType] = dict()
+        #        resultDict[serviceType].update(item)
+        #        resultDict.pop(key)
+        #return resultDict
+
+    @scanPortDecorator
+    def _scanServices(self, target, serviceList, showFiltered=False):
+        #argStr = '-p ' + ','.join([str(i) for i in serviceList])
+        #self.scanner.scan(hosts=target, arguments=argStr)
+        self.scanTcpPorts(target, serviceList)
+
 #-----------------------------------------------------------------------------
 def testCase(mode):
 
@@ -150,8 +213,20 @@ def testCase(mode):
         print(' - 5.Scan port range 22 - 30')
         rst = scanner.scanPortRange('172.18.178.6', (22,30), showFiltered=True )
         print('\t', rst)
+    elif mode == 3:
+        print(' - 6.Fast scan')
+        rst = scanner.fastScan('localhost')
+        print('\t', rst)
+    elif mode == 4:
+        print(' - 7.Scan service')
+        rst = scanner.scanServices('localhost', ['http', 'https'])
+        print('\t', rst)
+    elif mode == 5:
+        print(' - 7.Scan service')
+        rst = scanner.scanServices('sg.pool.ntp.org', ['ntp'])
+        print('\t', rst)
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 if __name__ == '__main__':
-    testCase(2)
+    testCase(4)
